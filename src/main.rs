77a2df6c4a91c36ec7ad4003c6dc9205d48a18d0
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-07-25 17:59:07 +0300

    feat: tarball fetching

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use async_recursion::async_recursion;
+use flate2::read::GzDecoder;
 use futures::future::join_all;
+use node_semver::{Range, Version};
 use reqwest::{Client, StatusCode};
-// use semver::{Version, VersionReq};
-use node_semver::{Version, Range};
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 use std::sync::Arc;
-use std::{collections::HashMap, env, fs, io::BufReader};
+use std::{
+    collections::HashMap,
+    env, fs,
+    io::{BufReader, Cursor},
+    path::Path,
+};
+use tar::Archive;
 use tokio::sync::Mutex;
 
 mod logger;
@@ -50,6 +57,7 @@ struct Dep {
 }
 
 const REGISTRY_URL: &str = "https://registry.npmjs.org";
+const NODE_MODULES: &str = "node_modules";
 
 fn parse_root_package() -> Package {
     let mut package_path = env::current_dir().expect("cannot get current dir");
@@ -99,6 +107,7 @@ async fn fetch_dep(dep: &Dep) -> Dependency {
     let client = Client::new();
 
     let url: String = format!("{REGISTRY_URL}/{}", dep.name);
+    // TODO: use json feature of reqwest
     let package = client
         .get(&url)
         .header("User-Agent", "Razee (Node Package Manger in Rust)")
@@ -148,9 +157,57 @@ async fn fetch_dep(dep: &Dep) -> Dependency {
     return dependency;
 }
 
+async fn fetch_tarball(dep_name: &String, dep_version: &String, dep_tarball: &String) {
+    let dep_dir = format!("{NODE_MODULES}/{dep_name}");
+    let dep_dir_package = format!("{dep_dir}/package.json");
+
+    if Path::new(&dep_dir_package).exists() {
+        return;
+    }
+
+    let client = Client::new();
+
+    let tarball_bytes = client
+        .get(dep_tarball)
+        .header("User-Agent", "Razee (Node Package Manger in Rust)")
+        .send()
+        .await
+        .unwrap()
+        .bytes()
+        .await
+        .unwrap();
+
+    let tarball_cursor = Cursor::new(tarball_bytes);
+    let tarball = GzDecoder::new(tarball_cursor);
+
+    let mut archive = Archive::new(tarball);
+
+    for entry in archive.entries().unwrap() {
+        if let Ok(mut entry) = entry {
+            let path = entry
+                .path()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .replace("package", &dep_dir)
+                .to_owned();
+
+            let mut folders: Vec<&str> = path.split("/").collect();
+            folders.pop();
+
+            if folders.len() > 1 {
+                fs::create_dir_all(folders.join("/")).unwrap();
+            }
+
+            entry.unpack(&path).unwrap();
+        }
+    }
+}
+
 #[async_recursion]
 async fn process_dep(dep: &Dep, processed_deps: ProcessedDeps) {
     let package = fetch_dep(&dep).await;
+    let tarball_promise = fetch_tarball(&package.name, &package.version, &package.dist.tarball);
 
     {
         let mut processed = processed_deps.lock().await;
@@ -174,6 +231,8 @@ async fn process_dep(dep: &Dep, processed_deps: ProcessedDeps) {
             }
         }
     }
+
+    tarball_promise.await;
 
     join_all(
         needs_processing
